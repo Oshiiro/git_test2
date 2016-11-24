d6daff44fdc4f9aa83a2e1c93428793bfdd12b08
Oshiiro <benjamin.delaborde@laposte.net>
2016-11-24 23:30:02 +0100

    ajout_du_dossier_type

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="css/bootstrap.min.css" media="screen" title="no title">
+    <link rel="stylesheet" href="css/font-awesome.min.css" media="screen" title="no title">
+    <link rel="stylesheet" href="css/style.css" media="screen" title="no title">
+    <title>Bootstrap</title>
+  </head>
+  <body>
+
+
+
+
+    <link rel="stylesheet" href="js/bootstrap.min.js" media="screen" title="no title">
+    <link rel="stylesheet" href="js/jquery-3.1.0.min.js" media="screen" title="no title">
+  </body>
+</html>
